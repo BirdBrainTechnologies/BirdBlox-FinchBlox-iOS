fc7be1b6edb4db319ab1c4e26ccf19b62c786d38
birdbraintech <tom@birdbraintechnologies.com>
2015-07-16 15:51:49 -0400

    Changed to checking for updates daily by comparing history file

--- Snap/Snap/DocumentManager.swift
+++ Snap/Snap/DocumentManager.swift
@@ -1,6 +1,6 @@
 //
 //  DocumentManager.swift
-//  Snap
+//  Snap for Hummingbird
 //
 //  Created by birdbrain on 7/13/15.
 //  Copyright (c) 2015 Birdbrain Technologies LLC. All rights reserved.
@@ -12,6 +12,7 @@ import Foundation
 let documentsPath: String = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)[0] as! String
 //let snapURL = NSURL(string: "http://snap.berkeley.edu/snapsource/snap.zip")
 let snapURL = NSURL(string: "https://github.com/jmoenig/Snap--Build-Your-Own-Blocks/archive/master.zip")
+let logURL = NSURL(string: "https://raw.githubusercontent.com/jmoenig/Snap--Build-Your-Own-Blocks/archive/master/history.txt")
 let zipPath = documentsPath.stringByAppendingPathComponent("temp.zip")
 let unzipPath = documentsPath.stringByAppendingPathComponent("snap")
 let lastUpdatePath = documentsPath.stringByAppendingPathComponent("log.txt")
@@ -22,17 +23,9 @@ public func getUpdate(){
     let zippedData = NSData(contentsOfURL: snapURL!)!
     zippedData.writeToFile(zipPath, atomically: true)
     Main.unzipFileAtPath(zipPath, toDestination: unzipPath)
-    if !fileManager.fileExistsAtPath(lastUpdatePath){
-        let dict: NSDictionary = NSDictionary(object: NSDate(), forKey: updateKey)
-        let newLog: NSData = NSKeyedArchiver.archivedDataWithRootObject(dict)
-        fileManager.createFileAtPath(lastUpdatePath, contents: newLog, attributes: nil)
-    }
     let cloudJS = String(contentsOfFile: getSnapPath().stringByAppendingPathComponent("cloud.js"), encoding: NSUTF8StringEncoding, error: nil)
     var localCloudJS = cloudJS?.stringByReplacingOccurrencesOfString("https://snap.apps.miosoft.com/SnapCloud", withString: "https://snap.apps.miosoft.com/SnapCloudLocal", options: NSStringCompareOptions.LiteralSearch, range: nil)
     localCloudJS?.writeToFile(getSnapPath().stringByAppendingPathComponent("cloud.js"), atomically: true, encoding: NSUTF8StringEncoding, error: nil)
-    let dict: NSDictionary = NSDictionary(object: NSDate(), forKey: updateKey)
-    let log: NSData = NSKeyedArchiver.archivedDataWithRootObject(dict)
-    log.writeToFile(lastUpdatePath, atomically: true)
 }
 
 public func getSnapPath() -> String {
@@ -40,16 +33,11 @@ public func getSnapPath() -> String {
 }
 
 public func shouldUpdate() -> Bool{
-    if !fileManager.fileExistsAtPath(zipPath){
-        let dict: NSDictionary = NSDictionary(object: NSDate(), forKey: updateKey)
-        let newLog: NSData = NSKeyedArchiver.archivedDataWithRootObject(dict)
-        fileManager.createFileAtPath(lastUpdatePath, contents: newLog, attributes: nil)
-        return true
-    }
-    if !fileManager.fileExistsAtPath(lastUpdatePath){
-        let dict: NSDictionary = NSDictionary(object: NSDate(), forKey: updateKey)
-        let newLog: NSData = NSKeyedArchiver.archivedDataWithRootObject(dict)
-        fileManager.createFileAtPath(lastUpdatePath, contents: newLog, attributes: nil)
+    let dictNew: NSDictionary = NSDictionary(object: NSDate(), forKey: updateKey)
+    let logFile: NSData = NSKeyedArchiver.archivedDataWithRootObject(dictNew)
+    logFile.writeToFile(lastUpdatePath, atomically: true)
+    
+    if !compareHistory() || !fileManager.fileExistsAtPath(lastUpdatePath){
         return true
     }
     let log = NSData(contentsOfFile: lastUpdatePath)
@@ -59,7 +47,7 @@ public func shouldUpdate() -> Bool{
     let dict: NSDictionary = NSKeyedUnarchiver.unarchiveObjectWithData(log!)! as! NSDictionary
     let date = dict.valueForKey(updateKey) as! NSDate
     let today = NSDate()
-    if (daysBetween(date, today) > 7){
+    if (daysBetween(date, today) > 1){
         return true
     }
     else{
@@ -74,3 +62,16 @@ private func daysBetween(d1: NSDate, d2: NSDate) -> Int{
     return components.day;
 }
 
+private func compareHistory() -> Bool{
+    let newHistory = String(contentsOfURL: logURL!, encoding: NSUTF8StringEncoding, error: nil)
+    let oldHistoryPath = getSnapPath().stringByAppendingPathComponent("history.txt")
+    if(!fileManager.fileExistsAtPath(oldHistoryPath)){
+        return false
+    }
+    let oldHistory = String(contentsOfFile: oldHistoryPath, encoding: NSUTF8StringEncoding, error: nil)
+    
+    if(oldHistory == newHistory){
+        return true
+    }
+    return false
+}
