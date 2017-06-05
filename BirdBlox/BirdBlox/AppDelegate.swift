5ef2dd55958003838debcc57c8a16054441dfc52
Jeremy Huang <jeremyhuang@cmu.edu>
2017-06-05 13:51:37 -0400

    Stopping the server when entering background seems to work better than stopping it when entering the foreground.

--- BirdBlox/BirdBlox/AppDelegate.swift
+++ BirdBlox/BirdBlox/AppDelegate.swift
@@ -54,12 +54,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+		if let vc = (window?.rootViewController as? ViewController) {
+			vc.backendServer.stop()
+		}
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
 		if let vc = (window?.rootViewController as? ViewController) {
-			vc.backendServer.stop()
 			vc.backendServer.start()
 		}
     }
